1b3a98626a57c4ca32b488673bfd5e51ad4646fe
Divya-Davuluri <davuluridivya9959@gmail.com>
2026-04-17 15:25:01 +0530

    Fix: Imported db in index.js for debug route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { initDb } = require('./src/models/db');
+const { db, initDb } = require('./src/models/db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
